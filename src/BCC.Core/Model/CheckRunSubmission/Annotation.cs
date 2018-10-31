43e67d910f4b1608609f172b4ffdfacf89ead447
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-10-31 16:27:48 -0400

    Missing objects and fields

--- src/BCC.Core/Model/CheckRunSubmission/Annotation.cs
+++ src/BCC.Core/Model/CheckRunSubmission/Annotation.cs
@@ -5,22 +5,22 @@
         public string Filename { get; set;  }
         public CheckWarningLevel CheckWarningLevel { get; set; }
         public string Title { get; set; }
+        public string RawDetails { get; set; }
         public string Message { get; set; }
-        public int LineNumber { get; set; }
+        public int StartLine { get; set; }
         public int EndLine { get; set; }
 
         public Annotation()
         {
         }
 
-        public Annotation(string filename, CheckWarningLevel checkWarningLevel, string title, string message,
-            int lineNumber, int endLine)
+        public Annotation(string filename,
+            int startLine, int endLine, CheckWarningLevel checkWarningLevel, string message)
         {
             Filename = filename;
             CheckWarningLevel = checkWarningLevel;
-            Title = title;
             Message = message;
-            LineNumber = lineNumber;
+            StartLine = startLine;
             EndLine = endLine;
         }
     }
